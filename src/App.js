77d6a671fa894291f50d16ace28857677c9b1af1
SvetoslavSO <sshaunin@mail.ru>
2023-05-02 21:38:19 +0300

    commit timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [timer, setValue] = useState('00:00:00');
+
+  function check(num) {
+    if(num < 10) {
+      return `0${num}`
+    } else if (num >= 10 && num <= 59) {
+      return `${num}`
+    } else return '00'
+  }
+
+  function checkHour (hour) {
+    if(hour < 10) {
+      return `0${hour}`
+    } else {
+      return `${hour}`
+    }
+  }
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        setValue(timer => {
+          let array = timer.split(":")
+          let curValSec = Number(array[2])+1;
+          let curValMin = Number(array[1]);
+          let curValHour = Number(array[0]);
+          let checkedSec = check(curValSec)
+          if(Number(checkedSec) === 0) {
+            let checkedMin = check(curValMin + 1)
+            if(Number(checkedMin) === 0) {
+              return (`${checkHour(curValHour + 1)}:00:00`)
+            } else {
+              return (`${checkHour(curValHour)}:${checkedMin}:00`)
+            } 
+          } else {
+            return (`${checkHour(curValHour)}:${check(curValMin)}:${checkedSec}`)
+          }
+        })
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timer">{ timer }</div>
     </div>
   );
 }
